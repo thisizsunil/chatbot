f91e3bdb571cb5070459b04ddb718b1d689b3a6c
jackal <realsuniladhikari66@gmail.com>
2025-03-22 12:45:04 +0545

    chatbot

--- lib/constant/api_constant.dart
+++ lib/constant/api_constant.dart
@@ -1,5 +1,5 @@
 class ApiContant {
-  static const String apiKey = "get your own key";
+  static const String apiKey = "get your own gemini key";
   static const String baseUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
 }
